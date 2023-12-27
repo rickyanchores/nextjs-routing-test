8196af8f9f49b5f2f9be0cea2ddc7b0ad107c93c
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-12-27 21:11:11 +0000

    install node sass

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className='Blog'>
+    <div className='Blog bg-red-700 text-white'>
         <h1>Blog</h1>
     </div>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='Home'>
+    <div className='Home bg-black text-white'>
         <h1>Home</h1>
     </div>
   )
